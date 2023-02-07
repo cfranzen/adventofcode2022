8253024ee8b0becb5b61ca06f176ac71847d2c5c
Christian Franzen <13965040+cfranzen@users.noreply.github.com>
2023-02-07 13:32:49 +0100

    Day 11: Fixed bug - do not reuse monkeys

--- src/main/java/de/cfranzen/adventofcode/day11/MonkeyBusinessApp.java
+++ src/main/java/de/cfranzen/adventofcode/day11/MonkeyBusinessApp.java
@@ -8,20 +8,22 @@ public class MonkeyBusinessApp {
 
     public static void main(String[] args) {
         final List<String> lines = new InputDownloader().downloadLines(2022, 11);
-        final InputParser parser = new InputParser();
-        final List<Monkey> monkeys = parser.parseMonkeys(lines);
-        final long lcm = parser.parseLCM(lines);
-        calculatePart1(monkeys);
-        calculatePart2(monkeys, lcm);
+        calculatePart1(lines);
+        calculatePart2(lines);
     }
 
-    private static void calculatePart1(final List<Monkey> monkeys) {
+    private static void calculatePart1(final List<String> lines) {
+        var monkeys = new InputParser().parseMonkeys(lines);
         var calculator = new MonkeyBusinessCalculator(monkeys);
         calculator.performMultipleRounds(20);
         System.out.println("Part 1: " + calculator.monkeyBusinessLevel());
     }
 
-    private static void calculatePart2(final List<Monkey> monkeys, final long lcm) {
+    private static void calculatePart2(final List<String> lines) {
+        var parser = new InputParser();
+        var monkeys = parser.parseMonkeys(lines);
+        var lcm = parser.parseLCM(lines);
+
         var calculator = new MonkeyBusinessCalculator(monkeys);
         calculator.performMultipleRounds(10000, wl -> wl.modulo(lcm));
         System.out.println("Part 2: " + calculator.monkeyBusinessLevel());
